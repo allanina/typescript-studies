b82d3b1643541d863d358be6af720219a6c14549
Allana Evellyn <allanaevellynm@gmail.com>
2022-03-17 22:23:19 -0300

    :pencil: modules 1 and 2

---
+++ microsoft-learn/module-02.js
@@ -0,0 +1,6 @@
+var x; //* Explicitly declares x as a number type
+var y = 1; //* Implicitly declares y as a number type
+var z; //* Declares z without initializing it (z is an variable typed as any)
+z = 1;
+z = "oi";
+console.log(z);
---
+++ microsoft-learn/module-02.ts
@@ -0,0 +1,6 @@
+let x: number;   //* Explicitly declares x as a number type
+let y = 1;       //* Implicitly declares y as a number type
+let z;           //* Declares z without initializing it (z is an variable typed as any)
+z = 1;
+z = "oi";
+console.log(z)
